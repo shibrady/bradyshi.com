b162db2efc34e054b2762064bd6c0fc0a30c516e
Brady Shi <brady-shi@outlook.com>
2016-12-27 00:12:17 -0800

    Added new project, addressed some projects styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,6 +16,25 @@ const Projects = () => {
           <p><b>Technologies Used</b>: React, Redux, Sass, Gulp, Node</p>
         </div>
       </section>
+      <section className="project-section">
+        <div className="project-image-cont">
+          <a href="https://www.youtube.com/watch?v=tBKmoV-hKBc">
+            <img src="./assets/proj_tankymctankface.png"
+              alt="Tanky McTankFace"/>
+          </a>
+        </div>
+        <div className="project-desc-cont">
+          <h1>Tanky McTankFace</h1>
+          <h2>
+            A group effort at understanding and
+            implementing fundamentals of computer graphics,
+            e.g. collision detection with bounding boxes, shadow mapping,
+            texture mapping, and so on.
+          </h2>
+          <h3>And having fun while doing it</h3>
+          <p><b>Technologies Used</b>: OpenGL, C++</p>
+        </div>
+      </section>
     </div>
   );
 };
