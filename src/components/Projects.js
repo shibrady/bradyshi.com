30c08990c7e3b7f89e162b3818bb37658b7679fd
Brady Shi <brady-shi@outlook.com>
2017-06-19 19:22:51 -0700

    Cleaned up Project component logic

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,24 +7,36 @@ class Projects extends React.Component {
     super(props);
   }
 
+  // Fetch projects upon opening of the projects page
   componentDidMount() {
     this.props.fetchProjects(process.env.NODE_ENV == 'development' ?
       'http://localhost:8080/projects' :
       'http://bradyshi.com/api/projects');
   }
 
+
   render() {
     let projects = this.props.projects;
+
+    // For each project we get from the server, create a separate container
     return (
       <div className="projects-cont">
         {projects.map((project) => {
-          if (project.image_type == 'video') {
+            // Return a different type of media container
+            // if the project uses a video instead of an image
+            let mediaElement = (project.image_type == 'video') ?
+            (<video controls>
+              <source src={'./assets/projects/' + project.image_filename}
+                type="video/mp4"/>
+            </video>) :
+            (<a href={project.extern}>
+              <img src={'./assets/projects/' + project.image_filename}
+                alt={project.name}/>
+            </a>);
+
             return (<section key={project._id} className="project-section">
               <div className="project-image-cont">
-                <video controls>
-                  <source src={'./assets/projects/' + project.image_filename}
-                    type="video/mp4"/>
-                </video>
+                {mediaElement}
               </div>
               <div className="project-desc-cont">
                 <h1>{project.name}</h1>
@@ -33,22 +45,6 @@ class Projects extends React.Component {
                 <p><b>Technologies Used</b>: {project.tech.join(', ')}</p>
               </div>
             </section>);
-          }
-
-          return (<section key={project._id} className="project-section">
-            <div className="project-image-cont">
-              <a href={project.extern}>
-                <img src={'./assets/projects/' + project.image_filename}
-                  alt={project.name}/>
-              </a>
-            </div>
-            <div className="project-desc-cont">
-              <h1>{project.name}</h1>
-              <h2>{project.description}</h2>
-              <h3>{project.subdesc}</h3>
-              <p><b>Technologies Used</b>: {project.tech.join(', ')}</p>
-            </div>
-          </section>);
         })}
       </div>
     );
